7de79f538328f81c56e78f76955ca58f6b48deb8
Amorsweetflips <mcserverpitufo@gmail.com>
2025-12-12 12:02:16 +0100

    Revert "fix(point-worker): compat view for point_award_jobs + point-queue shim"
    This reverts commit 03d2da28ada0d23d81a516bb5991e24bdbb6ecff.

--- lib/point-queue.ts
+++
@@ -1,3 +0,0 @@
-// Compatibility shim: older code/docs reference `lib/point-queue`.
-// The queue implementation was renamed to `lib/sweet-coin-queue` during the points -> sweet coins migration.
-export * from './sweet-coin-queue'
