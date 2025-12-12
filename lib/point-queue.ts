03d2da28ada0d23d81a516bb5991e24bdbb6ecff
Amorsweetflips <mcserverpitufo@gmail.com>
2025-12-12 12:00:16 +0100

    fix(point-worker): compat view for point_award_jobs + point-queue shim

---
+++ lib/point-queue.ts
@@ -0,0 +1,3 @@
+// Compatibility shim: older code/docs reference `lib/point-queue`.
+// The queue implementation was renamed to `lib/sweet-coin-queue` during the points -> sweet coins migration.
+export * from './sweet-coin-queue'
